1c758ee8ceab478d29bf028b56526414215c2d2f
kkkkkxiaofei <348564768@qq.com>
2022-04-10 16:53:27 +0800

    set ssr old way

---
+++ react-ssr-old-way/webpack.server.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './server/index.js',
+  target: 'node',
+  externals: [nodeExternals({
+    allowlist: ['react']
+  })],
+  output: {
+    path: path.resolve('server-build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // default
+              encoding: 'base64', // "utf8","utf16le","latin1","base64","hex","ascii","binary","ucs2"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
