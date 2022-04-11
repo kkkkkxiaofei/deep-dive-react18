871fa9a09dcccc09fd9de6479903a85cb9bc03da
kkkkkxiaofei <348564768@qq.com>
2022-04-11 22:29:22 +0800

    register callback after build

--- react-ssr-old-way/webpack.server.config.js
+++ react-ssr-old-way/webpack.server.config.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const { exec } = require("child_process");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: './server/index.js',
-  target: 'node',
+  entry: "./server/index.js",
+  target: "node",
   externals: [nodeExternals()],
   output: {
-    path: path.resolve('server-build'),
-    filename: 'index.js'
+    path: path.resolve("server-build"),
+    filename: "index.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
       {
         test: /\.scss$/i,
@@ -28,14 +29,26 @@ module.exports = {
         test: /\.svg$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               // default
-              encoding: 'base64', // "utf8","utf16le","latin1","base64","hex","ascii","binary","ucs2"
-            }
-          }
-        ]
-      }
-    ]
-  }
+              encoding: "base64", // "utf8","utf16le","latin1","base64","hex","ascii","binary","ucs2"
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+          exec("npm run build", (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      },
+    },
+  ],
 };
